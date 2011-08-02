112b1e78a1d764e827c634185f0232c1952453a3
zhuzhe <zhuzheiswho@sina.com>
2011-08-02 11:22:57 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icosplay::Application.config.secret_token = 'f9091e9f2db64c0a504bba3b96dedc933e0b2e802d306ef26488d38f06ab68164346cca2f06458f8e81ebb40bcb55bea940aa80e9ad6354cec5e3a8e788488f4'
