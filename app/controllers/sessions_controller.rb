2ad99706f9985f2ee493e7a62510ef0d09b652da
zhuzhe <zhuzheiswho@sina.com>
2011-10-20 10:49:08 +0800

    fix login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,19 @@
 class SessionsController < ApplicationController
   def new
-     session[:back_url] = request.referer
   end
 
   def create
-     @user = User.first(:conditions => {:email => params[:email], :pwd => params[:pwd]})
-     if @user
-       session[:user_id] = @user.id
-       redirect_to(session[:back_url] ? session[:back_url] : root_path)
-     else
-       redirect_to login_path
-     end
+    @user = User.first(:conditions => {:email => params[:email], :pwd => params[:pwd]})
+    if @user
+      session[:user_id] = @user.id
+      redirect_to(request.referer.match(/\/login/) ? root_path : request.referer)
+    else
+      redirect_to login_path
+    end
   end
 
   def destroy
-     session[:user_id] = nil
-     redirect_to root_path
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
